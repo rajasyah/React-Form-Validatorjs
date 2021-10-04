042a2404ab99feb20003d67c531c3fac47d218fb
rajasyah <rajasyahacp20@gmail.com>
2021-10-03 18:47:12 -0700

    add valiation with validationjs in form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import startup from "../assets/startup.png";
+import * as Validator from "validatorjs";
 
 const Form = () => {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const handleInput = (e) => {
+    setFullname(e.target.value);
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let data = {
+      fullname,
+      email,
+      password,
+    };
+
+    let rules = {
+      fullname: "required",
+      email: "required|email",
+      password: "required|min:8",
+    };
+    let validation = new Validator(data, rules);
+    validation.passes();
+    setErrors([
+      ...validation.errors.get("fullname"),
+      ...validation.errors.get("email"),
+      ...validation.errors.get("password"),
+    ]);
+    if (errors.length === 0) {
+      alert(`
+                    fullname : ${fullname}
+                    email : ${email}
+                    password : ${password}
+                `);
+    }
+    console.log(errors);
+  };
+
   return (
     <div className="w-11/12 h-5/6 flex justify-between items-center shadow-2xl ">
       <div className="bg-blue-300 flex flex-col w-6/12 h-full shadow-inner  rounded-md">
@@ -21,7 +69,7 @@ const Form = () => {
             />
           </svg>
         </div>
-        <div className="text-center mt-12 text-white text-xl">
+        <div className="text-center mt-12 text-gray-50 text-xl">
           <h1>
             Mari Bergabung <br />
             Untuk Mendapatkan lebih banyak keuntungan
@@ -31,13 +79,51 @@ const Form = () => {
           <img src={startup} alt="startup" className="w-96" />
         </div>
       </div>
-      <div className="w-6/12 h-full bg-gray-700 rounded-md">
-        <div className="button flex justify-end m-5 cursor-pointer">
-          <button className="px-3 py-1 border-2 border-gray-300 text-gray-300 rounded-md shadow-md hover:bg-blue-300">
-            Login
-          </button>
-          <button></button>
-        </div>
+      <div className="w-6/12 h-full flex flex-col justify-center bg-gray-700 rounded-md">
+        <ul className="mx-16 text-red-600">
+          {errors.map((e, i) => (
+            <li key={i}>{e}</li>
+          ))}
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <div className="form flex flex-col gap-5 mx-16 text-white ">
+            <div className="flex flex-col">
+              <label htmlFor="fullname">Full Name</label>
+              <input
+                type="text"
+                id="fullname"
+                placeholder="Enter your full name"
+                className="bg-transparent border-b-2 focus:outline-none"
+                onChange={handleInput}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                className="bg-transparent border-b-2 focus:outline-none"
+                onChange={handleEmail}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                onChange={handlePassword}
+                className="bg-transparent border-b-2 focus:outline-none"
+              />
+            </div>
+          </div>
+          <div className="w-20 text-center mx-16 mt-10 cursor-pointer">
+            <button className="btn" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
